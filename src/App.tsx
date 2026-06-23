d26d0516e1c97fc3d2ab62d5b64bfda0e955a3ba
tlsql1128 <tlsql11282@gmail.com>
2026-06-23 17:17:35 +0900

    Revert "1"
    This reverts commit 3b60e5708340c1077d66c39f8f93b631cfb0c1c3.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import MainPage from './pages/MainPage'
 import ReservationPage from './pages/Reservation/ReservationPage'
 import NoticeDetailPage from './pages/Notice/NoticeDetailPage'
 import NoticePage from './pages/Notice/NoticePage'
-import BrandStoryPage from './pages/BrandStory'
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/preview" element={<ComponentPreview />} />
         <Route path="/class/introduction" element={<ClassIntroductionPage />} />
         <Route path="/reservation" element={<ReservationPage />} />
-        <Route path="/brand/story" element={<BrandStoryPage />} />
         <Route path="/event/notice" element={<NoticePage />} />
         <Route path="/event/notice/:noticeId" element={<NoticeDetailPage />} />
         <Route path="*" element={<Navigate to="/preview" replace />} />
