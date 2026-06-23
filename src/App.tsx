8d799ec76d9e071e9f0b968b4764291d7dc4b3f6
hyeing2 <hyeing524@gmail.com>
2026-06-23 17:01:43 +0900

    경로 오류 수정
    중복 경로 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ReservationPage from './pages/Reservation/ReservationPage'
 import NoticeDetailPage from './pages/Notice/NoticeDetailPage'
 import NoticePage from './pages/Notice/NoticePage'
 import LoginPage from './pages/Login/LoginPage'
+import SeasonTeaPage from './pages/SeasonTea/SeasonTeaPage'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/event/notice" element={<NoticePage />} />
           <Route path="/event/notice/:noticeId" element={<NoticeDetailPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/product/season-tea" element={<SeasonTeaPage />} />
           <Route path="*" element={<Navigate to="/preview" replace />} />
         </Routes>
       </AuthProvider>
